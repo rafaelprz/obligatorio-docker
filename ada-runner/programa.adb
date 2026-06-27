93e6af7ce350cfcabfc9a1c26855646769ce3170
Rafael Perez <raffaelprz@gmail.com>
2026-06-27 20:45:06 -0300

    Initial commits con proyectos demo

---
+++ ada-runner/programa.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+procedure Programa is
+begin
+   Put_Line("Hola desde Ada Runner");
+end Programa;
---
+++ c-runner/programa.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hola desde C Runner\n");
+    return 0;
+}
